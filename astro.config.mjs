0100ee30b88fb026a4a16096795778aa5ec23482
LucaSain <lucasainenco2@gmail.com>
2024-02-23 17:05:32 +0200

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), mdx()],
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 });
